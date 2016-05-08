cb1238e905e5487551677fcef7d7d7c929b42081
Facundo Olano <facundo.olano@gmail.com>
2016-05-07 22:04:51 -0300

    more es6 goodies

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-module.exports = require('./lib/google-play-scraper');
+const c = require('./lib/constants');
+
+module.exports.category = c.category;
+module.exports.collection = c.collection;
+module.exports.sort = c.sort;
+module.exports.age = c.age;
+
+module.exports.app = require('./lib/app');
+module.exports.list = require('./lib/list');
+module.exports.search = require('./lib/search');
+module.exports.suggest = require('./lib/suggest');
+module.exports.developer = require('./lib/developer');
+module.exports.reviews = require('./lib/reviews');
+module.exports.similar = require('./lib/similar');
