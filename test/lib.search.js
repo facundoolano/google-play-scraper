0e112279e6fc4e42c3a642c6d6ae5445c99ac355
Afoninsky <vkfont@gmail.com>
2016-05-11 13:58:37 +0300

    fix search.num + append according test

--- test/lib.search.js
+++ test/lib.search.js
@@ -2,10 +2,25 @@
 
 const gplay = require('../index');
 const assertValidApp = require('./common').assertValidApp;
+const assert = require('chai').assert;
 
 describe('Search method', () => {
   it('should fetch a valid application list', () => {
     return gplay.search({term: 'Panda vs Zombies'})
     .then((apps) => apps.map(assertValidApp));
+  });
+
+  it('should validate the results number', function () {
+    this.timeout(5000);
+
+    const count = 100;
+    return gplay.search({
+      term: 'vr',
+      num: count
+    })
+    .then((apps) => {
+      apps.map(assertValidApp);
+      assert(apps.length === count, `should return ${count} items but ${apps.length} returned`);
+    });
   });
 });
