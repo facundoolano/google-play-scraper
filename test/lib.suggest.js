3b37160a032ffd62b44d24fa4a06b00c3370ebcc
Facundo Olano <facundo.olano@gmail.com>
2016-05-28 21:23:22 -0300

    test

--- test/lib.suggest.js
+++ test/lib.suggest.js
@@ -5,7 +5,7 @@ const assert = require('chai').assert;
 
 describe('Suggest method', () => {
   it('should return five suggestion for a common term', () => {
-    return gplay.suggest('panda')
+    return gplay.suggest('p')
     .then((results) => {
       assert.equal(results.length, 5, `expected ${results} to have 5 elements`);
       results.map((r) => assert.include(r, 'panda'));
