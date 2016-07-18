45a8791129d60f587bc91253d405f84ad3c97190
Facundo Olano <facundo.olano@gmail.com>
2016-07-17 21:05:51 -0300

    simplify unit tests

--- test/lib.app.js
+++ test/lib.app.js
@@ -31,8 +31,10 @@ describe('App method', () => {
 
         assert.isString(app.version);
         assert.isString(app.size);
-        assert.isString(app.androidVersion);
         assert.isString(app.contentRating);
+
+        assert.equal(app.androidVersion, '2.3');
+        assert.equal(app.androidVersionText, '2.3 and up');
 
         assert.equal(app.price, '0');
         assert(app.free);
@@ -55,173 +57,39 @@ describe('App method', () => {
       });
   });
 
-  const languages = [
-    ['de', 'de'],
-    ['en', 'au'],
-    ['en', 'in'],
-    ['en', 'us'],
-    ['en', 'gb'],
-    ['ar'],
-    ['bg'],
-    ['zh', 'hk'],
-    ['zh', 'cn'],
-    ['zh', 'tw'],
-    ['ko', 'kr'],
-    ['hr'],
-    ['da', 'dk'],
-    ['es', '419'],
-    ['es', 'es'],
-    ['es', 'us'],
-    ['et'],
-    ['fr', 'ca'],
-    ['fr', 'fr'],
-    ['fi', 'fi'],
-    ['el', 'gr'],
-    ['iw', 'il'],
-    ['hi', 'in'],
-    ['hu', 'hu'],
-    ['is', 'is'],
-    ['it', 'it'],
-    ['ja', 'jp'],
-    ['lv'],
-    ['lt'],
-    ['nl', 'nl'],
-    ['no', 'no'],
-    ['pl', 'pl'],
-    ['pt', 'br'],
-    ['pt', 'pt'],
-    ['ro'],
-    ['ru', 'ru'],
-    ['sr'],
-    ['sk'],
-    ['sl'],
-    ['sv', 'se'],
-    ['cs', 'cz'],
-    ['th'],
-    ['tr', 'tr'],
-    ['uk'],
-    ['vi']
-  ];
+  it('should properly parse a VARY android version', () => {
+    return gplay.app({appId: 'com.facebook.katana'})
+      .then((app) => {
+        assert.equal(app.androidVersion, 'VARY');
+        assert.equal(app.androidVersionText, 'Varies with device');
+      });
+  });
 
-  it('should properly parse a VARY android version');
+  it('should fetch app in spanish', () => {
+    return gplay.app({appId: 'com.dxco.pandavszombies', lang: 'es', country: 'ar'})
+      .then((app) => {
+        assert.equal(app.appId, 'com.dxco.pandavszombies');
+        assert.equal(app.title, 'Panda vs Zombies');
+        assert.equal(app.url, 'https://play.google.com/store/apps/details?id=com.dxco.pandavszombies&hl=es&gl=ar');
+        assert.isNumber(app.minInstalls);
+        assert.isNumber(app.maxInstalls);
 
-  it('should properly parse a VARY android version');
+        assert.equal(app.androidVersion, '2.3');
+        assert.equal(app.androidVersionText, '2.3 y versiones superiores');
+      });
+  });
 
-  // TODO test iw_il, is_is
+  it('should fetch app in french', () => {
+    return gplay.app({appId: 'com.dxco.pandavszombies', lang: 'fr', country: 'fr'})
+      .then((app) => {
+        assert.equal(app.appId, 'com.dxco.pandavszombies');
+        assert.equal(app.title, 'Panda vs Zombies');
+        assert.equal(app.url, 'https://play.google.com/store/apps/details?id=com.dxco.pandavszombies&hl=fr&gl=fr');
+        assert.isNumber(app.minInstalls);
+        assert.isNumber(app.maxInstalls);
 
-  // Facebook
-  for (let i in languages) {
-    let lang = languages[i][0];
-    let country = typeof languages[i][1] !== 'undefined' ? languages[i][1] : lang;
-
-    it.skip('should fetch valid application data in ' + lang + '_' + country, () => {
-      return gplay.app({appId: 'com.facebook.katana', lang: lang, country: country})
-        .then((app) => {
-          assert.equal(app.appId, 'com.facebook.katana');
-          assert.equal(app.title, 'Facebook');
-          assert.equal(app.url, 'https://play.google.com/store/apps/details?id=com.facebook.katana&hl=' + lang + '&gl=' + country);
-          assertValidUrl(app.icon);
-
-          assert.isNumber(app.score);
-          assert(app.score > 0);
-          assert(app.score <= 5);
-
-          assert.isNumber(app.minInstalls);
-          assert.isNumber(app.maxInstalls);
-          assert.isNumber(app.reviews);
-
-          assert.isString(app.summary);
-          assert.isString(app.description);
-          assert.isString(app.descriptionHTML);
-          assert.isString(app.updated);
-          assert.equal(app.genreId, 'SOCIAL');
-
-          assert.isString(app.version);
-          assert.isString(app.size);
-          assert.equal(app.androidVersion, 'VARY');
-          assert.isString(app.androidVersionText);
-          assert.isString(app.contentRating);
-
-          assert.equal(app.price, '0');
-          assert(app.free);
-
-          assert.equal(app.developer, 'Facebook');
-          assertValidUrl(app.developerWebsite);
-          assert(validator.isEmail(app.developerEmail), `${app.developerEmail} is not an email`);
-
-          if (app.video) {
-            assertValidUrl(app.video);
-          }
-
-          ['1', '2', '3', '4', '5'].map((v) => assert.property(app.histogram, v));
-
-          assert(app.screenshots.length);
-          app.screenshots.map(assertValidUrl);
-
-          assert(app.comments.length);
-          app.comments.map(assert.isString);
-
-          assert(app.recentChanges.length);
-          app.recentChanges.map(assert.isString);
-        });
-    });
-  }
-
-  // What's App
-  for (let i in languages) {
-    let lang = languages[i][0];
-    let country = typeof languages[i][1] !== 'undefined' ? languages[i][1] : lang;
-
-    it.skip('should fetch valid application data in ' + lang + '_' + country, () => {
-      return gplay.app({appId: 'com.whatsapp', lang: lang, country: country})
-        .then((app) => {
-          assert.equal(app.appId, 'com.whatsapp');
-          assert.equal(app.title, 'WhatsApp Messenger');
-          assert.equal(app.url, 'https://play.google.com/store/apps/details?id=com.whatsapp&hl=' + lang + '&gl=' + country);
-          assertValidUrl(app.icon);
-
-          assert.isNumber(app.score);
-          assert(app.score > 0);
-          assert(app.score <= 5);
-
-          assert.isNumber(app.minInstalls);
-          assert.isNumber(app.maxInstalls);
-          assert.isNumber(app.reviews);
-
-          assert.isString(app.summary);
-          assert.isString(app.description);
-          assert.isString(app.descriptionHTML);
-          assert.isString(app.updated);
-          assert.equal(app.genreId, 'COMMUNICATION');
-
-          assert.isString(app.version);
-          assert.isString(app.size);
-          assert.equal(app.androidVersion, '2.1');
-          assert.isString(app.androidVersionText);
-          assert.isString(app.contentRating);
-
-          assert.equal(app.price, '0');
-          assert(app.free);
-
-          assert.equal(app.developer, 'WhatsApp Inc.');
-          assertValidUrl(app.developerWebsite);
-          assert(validator.isEmail(app.developerEmail), `${app.developerEmail} is not an email`);
-
-          if (app.video) {
-            assertValidUrl(app.video);
-          }
-
-          ['1', '2', '3', '4', '5'].map((v) => assert.property(app.histogram, v));
-
-          assert(app.screenshots.length);
-          app.screenshots.map(assertValidUrl);
-
-          assert(app.comments.length);
-          app.comments.map(assert.isString);
-
-          assert(app.recentChanges.length);
-          app.recentChanges.map(assert.isString);
-        });
-    });
-  }
+        assert.equal(app.androidVersion, '2.3');
+        assert.equal(app.androidVersionText, '2.3 ou version ultérieure');
+      });
+  });
 });
