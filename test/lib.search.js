f2a1b21e36739db3e708450b2d83c46f12d98b7d
Alexei Mikhailov <alemikh@gmail.com>
2017-04-05 14:14:14 +0300

    Add test case
    Covers the case when we start from non-cluster of subsections page

--- test/lib.search.js
+++ test/lib.search.js
@@ -29,4 +29,11 @@ describe('Search method', () => {
        assert(apps.length === 155, 'should return as many apps as requested');
        assert(R.uniq(apps).length === 155, 'should return distinct apps');
      }));
+
+  it('should fetch multiple pages of when not starting from cluster of subsections', () =>
+     gplay.search({term: 'clash of clans', num: 65})
+     .then((apps) => {
+       assert(apps.length === 65, 'should return as many apps as requested');
+       assert(R.uniq(apps).length === 65, 'should return distinct apps');
+     }));
 });
