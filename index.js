a62ce5b668a7e0154b158bbbe700357ab68e6b81
Ícaro Ponce <ponceicaro@gmail.com>
2017-06-09 19:27:05 -0300

    created optional memoization method

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
+'use strict';
+
 const c = require('./lib/constants');
+const R = require('ramda');
+const memoizee = require('memoizee');
 
-module.exports.category = c.category;
-module.exports.collection = c.collection;
-module.exports.sort = c.sort;
-module.exports.age = c.age;
+const constants = {
+  category: c.category,
+  collection: c.collection,
+  sort: c.sort,
+  age: c.age
+};
 
-module.exports.app = require('./lib/app');
-module.exports.list = require('./lib/list');
-module.exports.search = require('./lib/search');
-module.exports.suggest = require('./lib/suggest');
-module.exports.developer = require('./lib/developer');
-module.exports.reviews = require('./lib/reviews');
-module.exports.similar = require('./lib/similar');
-module.exports.permissions = require('./lib/permissions');
+const methods = {
+  app: require('./lib/app'),
+  list: require('./lib/list'),
+  search: require('./lib/search'),
+  suggest: require('./lib/suggest'),
+  developer: require('./lib/developer'),
+  reviews: require('./lib/reviews'),
+  similar: require('./lib/similar'),
+  permissions: require('./lib/permissions')
+};
+
+function memoized (opts) {
+  const cacheOpts = Object.assign({
+    primitive: true,
+    normalizer: JSON.stringify,
+    maxAge: 1000 * 60 * 5, // cache for 5 minutes
+    max: 1000 // save up to 1k results to avoid memory issues
+  }, opts);
+  const doMemoize = (fn) => memoizee(fn, cacheOpts);
+  return Object.assign({}, constants, R.map(doMemoize, methods));
+}
+
+module.exports = Object.assign({memoized}, constants, methods);
