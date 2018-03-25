c03d011ffdf4a1b9795a98c8973d76c77c24a0bf
Facundo Olano <facundo.olano@gmail.com>
2018-03-25 11:40:31 -0300

    accept uppercase suggestions in test

--- test/lib.suggest.js
+++ test/lib.suggest.js
@@ -7,6 +7,6 @@ describe('Suggest method', () => {
   it('should return five suggestion for a common term', () => gplay.suggest({term: 'p'})
     .then((results) => {
       assert.equal(results.length, 5, `expected ${results} to have 5 elements`);
-      results.map((r) => assert.include(r, 'p'));
+      results.map((r) => assert.include(r.toLowerCase(), 'p'));
     }));
 });
