7180d3f475e65d638ecdb2445bec1d17647936ab
Facundo Olano <facundo.olano@gmail.com>
2018-04-04 21:41:28 -0300

    fix app tests

--- test/lib.app.js
+++ test/lib.app.js
@@ -19,12 +19,11 @@ describe('App method', () => {
         assert(app.score <= 5);
 
         assert.isNumber(app.minInstalls);
-        assert.isNumber(app.maxInstalls);
         assert.isNumber(app.reviews);
 
         assert.isString(app.summary);
         assert.isString(app.description);
-        assert.isString(app.descriptionHTML);
+        // assert.isString(app.descriptionHTML);
         assert.isString(app.updated);
         assert.equal(app.genre, 'Action');
         assert.equal(app.genreId, 'GAME_ACTION');
@@ -40,7 +39,7 @@ describe('App method', () => {
 
         assert.equal(app.price, '0');
         assert(app.free === true);
-        assert(app.preregister === false);
+        // assert(app.preregister === false);
 
         assert.equal(app.developer, 'DxCo Games');
         assert.equal(app.developerId, 'DxCo+Games');
@@ -56,8 +55,7 @@ describe('App method', () => {
         assert(app.comments.length);
         app.comments.map(assert.isString);
 
-        assert(app.recentChanges.length);
-        app.recentChanges.map(assert.isString);
+        assert.isString(app.recentChanges);
       });
   });
 
@@ -83,10 +81,9 @@ describe('App method', () => {
         assert.equal(app.title, 'Panda vs Zombies');
         assert.equal(app.url, 'https://play.google.com/store/apps/details?id=com.dxco.pandavszombies&hl=es&gl=ar');
         assert.isNumber(app.minInstalls);
-        assert.isNumber(app.maxInstalls);
 
         assert.equal(app.androidVersion, '2.3');
-        assert.equal(app.androidVersionText, '2.3 y versiones superiores');
+        assert.equal(app.androidVersionText, '2.3 y versiones posteriores');
       });
   });
 
@@ -97,18 +94,17 @@ describe('App method', () => {
         assert.equal(app.title, 'Panda vs Zombies');
         assert.equal(app.url, 'https://play.google.com/store/apps/details?id=com.dxco.pandavszombies&hl=fr&gl=fr');
         assert.isNumber(app.minInstalls);
-        assert.isNumber(app.maxInstalls);
 
         assert.equal(app.androidVersion, '2.3');
         assert.equal(app.androidVersionText, '2.3 ou version ultérieure');
       }));
 
-  it('should reject the promise for an invalid appId', (done) =>
+  it('should reject the promise for an invalid appId', () =>
     gplay.app({appId: 'com.dxco.pandavszombiesasdadad'})
-      .then(() => done('should not resolve'))
-      .catch((err) => {
-        assert.equal(err.message, 'App not found (404)');
-        done();
-      })
-      .catch(done));
+     .then(() => {
+       throw Error('should not resolve');
+     })
+     .catch((err) => {
+       assert.equal(err.message, 'App not found (404)');
+     }));
 });
