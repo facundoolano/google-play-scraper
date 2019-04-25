a4f2874a870dea8a5f65b5b7f0b02f6558b9ac97
hunnapikes <serioga19.96@gmail.com>
2019-04-25 13:26:35 +0300

    oops, fix similar test

--- test/lib.similar.js
+++ test/lib.similar.js
@@ -6,5 +6,6 @@ const assertValidApp = require('./common').assertValidApp;
 describe('Similar method', () => {
   it('should fetch a valid application list', () => {
     return gplay.similar({ appId: 'com.mojang.minecraftpe' })
+      .then((apps) => apps.map(assertValidApp));
   });
 });
