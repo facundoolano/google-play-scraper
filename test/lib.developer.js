2aa1326ac4b4c4f4117c59e5f4be8ce7674eda3e
Suvitruf <suvitruf@gmail.com>
2019-08-17 14:03:08 +0300

    full apps detail on developers api

--- test/lib.developer.js
+++ test/lib.developer.js
@@ -6,30 +6,29 @@ const assertValidApp = require('./common').assertValidApp;
 const R = require('ramda');
 
 describe('Developer method', () => {
-  // it('should fetch a valid application list for the given developer', () => {
-  //   return gplay.developer({ devId: 'Jam City, Inc.' })
-  //     .then((apps) => apps.map(assertValidApp))
-  //     .then((apps) => apps.map((app) => assert.equal(app.developer, 'Jam City, Inc.')));
-  // });
-  //
-  // it('should fetch several pages of distinct apps for the developer with number id', () =>
-  //   gplay.developer({ devId: '5700313618786177705', num: 77, fullDetail: true })
-  //     .then((apps) => {
-  //       assert.equal(apps.length, 77, 'should return as many apps as requested');
-  //       assert.equal(R.uniq(apps).length, 77, 'should return distinct apps');
-  //     }));
+  it('should fetch a valid application list for the given developer', () => {
+    return gplay.developer({ devId: 'Jam City, Inc.' })
+      .then((apps) => apps.map(assertValidApp))
+      .then((apps) => apps.map((app) => assert.equal(app.developer, 'Jam City, Inc.')));
+  });
 
-  it('should fetch several pages of distinct apps for the developer with string id', () =>
-    gplay.developer({ devId: 'Google LLC', num: 7, fullDetail: true })
+  it('should fetch several pages of distinct apps for the developer with number id', () =>
+    gplay.developer({ devId: '5700313618786177705', num: 77, fullDetail: true })
       .then((apps) => {
-        console.log(apps);
-        assert.equal(apps.length, 7, 'should return as many apps as requested');
-        assert.equal(R.uniq(apps).length, 7, 'should return distinct apps');
+        assert.equal(apps.length, 77, 'should return as many apps as requested');
+        assert.equal(R.uniq(apps).length, 77, 'should return distinct apps');
       }));
 
-  // it('should not throw an error if too many apps requested', () =>
-  //   gplay.developer({ devId: 'Google LLC', num: 500 })
-  //     .then((apps) => {
-  //       assert(apps.length >= 100, 'should return as many apps as availabe');
-  //     }));
+  it('should fetch several pages of distinct apps for the developer with string id', () =>
+    gplay.developer({ devId: 'Google LLC', num: 77, fullDetail: true })
+      .then((apps) => {
+        assert.equal(apps.length, 77, 'should return as many apps as requested');
+        assert.equal(R.uniq(apps).length, 77, 'should return distinct apps');
+      }));
+
+  it('should not throw an error if too many apps requested', () =>
+    gplay.developer({ devId: 'Google LLC', num: 500 })
+      .then((apps) => {
+        assert(apps.length >= 100, 'should return as many apps as availabe');
+      }));
 });
