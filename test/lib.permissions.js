8a045b324e2836ebb6f88c92d0db48a88ef96353
Icaro Caldeira <icaro.caldeira@hotmail.com>
2019-10-07 10:32:53 -0300

    :bug: fix permission method (#357)

--- test/lib.permissions.js
+++ test/lib.permissions.js
@@ -14,8 +14,25 @@ describe('Permissions method', () => {
         });
       }));
 
+  it('should return an array of permissions and descriptions for different response format', () =>
+    gplay.permissions({ appId: 'air.tv.ingames.cubematch.free' })
+      .then((results) => {
+        assert(results.length);
+        results.map((perm) => {
+          assert.isString(perm.permission);
+          assert.isString(perm.type);
+        });
+      }));
+
   it('should return skip descriptions if short option is passed', () =>
     gplay.permissions({ appId: 'com.sgn.pandapop.gp', short: true })
+      .then((results) => {
+        assert(results.length);
+        results.map(assert.isString);
+      }));
+
+  it('should return skip descriptions if short option is passed for different response format', () =>
+    gplay.permissions({ appId: 'air.tv.ingames.cubematch.free', short: true })
       .then((results) => {
         assert(results.length);
         results.map(assert.isString);
