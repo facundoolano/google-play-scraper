834f236b421a4efc6ade211d768d8ed73e7f7bdd
Rafail Antoniadis <rafail.antoniadis@mway.io>
2022-06-29 00:04:59 +0200

    test more mapping with and without country in options

--- test/lib.search.js
+++ test/lib.search.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const gplay = require('../index');
-const assertValidApp = require('./common').assertValidApp;
+const { assertValidApp, assertIdsInArray } = require('./common');
 const assert = require('chai').assert;
 
 describe('Search method', () => {
@@ -61,22 +61,41 @@ describe('Search method', () => {
           .then((apps) => apps.map(assertValidApp));
       });
     });
+  });
 
-    describe('with more results section', () => {
-      it('should fetch a valid application list for non eu country', () => {
-        return gplay.search({ term: 'Facebook', country: 'GE' })
-          .then((apps) => apps.map(assertValidApp));
-      });
+  describe('more results mapping', () => {
+    it('schould return few netflix apps', () => {
+      return gplay.search({ term: 'netflix' })
+        .then((apps) => {
+          assert.equal(apps[0].appId, 'com.netflix.mediaclient');
+          assertIdsInArray(apps, 'com.netflix.ninja', 'com.netflix.NGP.StrangerThings');
+        });
+    });
 
-      it('should fetch a valid application list for eu country', () => {
-        return gplay.search({ term: 'Facebook', country: 'IT' })
-          .then((apps) => apps.map(assertValidApp));
-      });
+    it('should return few netflix apps from german store with german language', () => {
+      return gplay.search({ term: 'netflix', lang: 'de', country: 'DE' })
+        .then((apps) => {
+          assert.equal(apps[0].appId, 'com.netflix.mediaclient');
+          assertIdsInArray(apps, 'com.netflix.ninja', 'com.netflix.NGP.StrangerThings');
+        });
+    });
 
-      it('should fetch a valid application list for eu country with specific language', () => {
-        return gplay.search({ term: 'Facebook', country: 'IT', lang: 'de' })
-          .then((apps) => apps.map(assertValidApp));
-      });
+    it('should reutrn few google mail apps', () => {
+      return gplay.search({ term: 'gmail' })
+        .then((apps) => {
+          assert.equal(apps[0].appId, 'com.google.android.gm');
+          assertIdsInArray(apps, 'com.google.android.gm.lite', 'com.google.android.apps.docs');
+        });
+    });
+
+    it('should return apps for search with a category as query', () => {
+      return gplay.search({ term: 'games' })
+        .then((apps) => assertIdsInArray(apps, 'com.kiloo.subwaysurf'));
+    });
+
+    it('should throw if no result returned', () => {
+      return gplay.search({ term: 'asdasdyxcnmjysalsaflaslf' })
+        .catch((error) => assert.isNotEmpty(error.message));
     });
   });
 });
