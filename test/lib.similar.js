3662b79e05664d0164bc3d9b2c5d02cfdfaa1c73
Dario Bravo <dbravo@basecodeit.com>
2022-07-03 20:20:56 -0300

    Fixed linter issue

--- test/lib.similar.js
+++ test/lib.similar.js
@@ -12,6 +12,6 @@ describe('Similar method', () => {
 
   it('should fetch games from different developers', () => {
     return gplay.similar({ appId: 'com.mojang.minecraftpe' })
-      .then((apps) => assert.isTrue(apps.some(app => app.developer != apps[0].developer)));
+      .then((apps) => assert.isTrue(apps.some(app => app.developer !== apps[0].developer)));
   });
 });
