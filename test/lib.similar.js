cf78b9cec9a241726575601ba8982b6612913b8a
dbravobasecodeit <33032037+dbravobasecodeit@users.noreply.github.com>
2022-07-18 10:33:03 -0300

    Fixed similar method to support games (#570)

--- test/lib.similar.js
+++ test/lib.similar.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const assert = require('chai').assert;
 const gplay = require('../index');
 const assertValidApp = require('./common').assertValidApp;
 
@@ -7,5 +8,10 @@ describe('Similar method', () => {
   it('should fetch a valid application list', () => {
     return gplay.similar({ appId: 'com.mojang.minecraftpe' })
       .then((apps) => apps.map(assertValidApp));
+  });
+
+  it('should fetch games from different developers', () => {
+    return gplay.similar({ appId: 'com.mojang.minecraftpe' })
+      .then((apps) => assert.isTrue(apps.some(app => app.developer !== apps[0].developer)));
   });
 });
