93ce5df6bb13a4f1e1ef20464e97357c4e38715d
jbigman <jbigman@users.noreply.github.com>
2023-08-03 18:18:44 +0200

    Fixed throttle tests (add mocks and updated expected results)

--- test/utils.throttle.js
+++ test/utils.throttle.js
@@ -1,25 +1,37 @@
 const requestLib = require('got');
 const throttled = require('../lib/utils/throttle');
+const sinon = require('sinon');
+const assert = require('chai').assert;
 
-it('Should make three requests with 5000ms interval. (Throttle function)', function (done) {
-  this.timeout(30000);
-  const req = throttled(requestLib, {
-    limit: 1,
-    interval: 5000
+describe('Throttle tests', function () {
+  let server;
+  this.timeout(15000);
+
+  before(function () {
+    server = sinon.fakeServer.create();
+  });
+  after(function () {
+    server.restore();
   });
 
-  Promise.all([req({ url: 'https://yesno.wtf/api' }), req({ url: 'https://yesno.wtf/api' }), req({ url: 'https://yesno.wtf/api' })])
-    .then((response) => response.map(req => new Date(req.headers.date).getTime()))
-    .then((dates) => {
-      const firstAndSecondReq = dates[1] - dates[0];
-      const secondAndThirdReq = dates[2] - dates[1];
-      if (
-        (firstAndSecondReq >= 5000 && firstAndSecondReq <= 6500) &&
-        (secondAndThirdReq >= 5000 && secondAndThirdReq <= 6500)
-      ) {
-        done();
-      } else {
-        throw new Error('Wrong interval between requests.');
-      }
+  const url = 'https://yesno.wtf/api';
+
+  it('Should make three requests with 5000ms interval. (Throttle function)', function () {
+    server.respondWith('GET', url, JSON.stringify({ test: 'this works' }));
+    const req = throttled(requestLib, {
+      limit: 1,
+      interval: 5000
     });
+    return Promise.all([req({ url }), req({ url }), req({ url })])
+      .then((response) => response.map(req => new Date(req.headers.date).getTime()))
+      .then((dates) => {
+        const firstAndSecondReq = dates[1] - dates[0];
+        const secondAndThirdReq = dates[2] - dates[1];
+
+        assert.isAtLeast(firstAndSecondReq, 5000);
+        assert.isAtMost(firstAndSecondReq, 6500);
+        assert.isAtLeast(secondAndThirdReq, 5000);
+        assert.isAtMost(secondAndThirdReq, 6500);
+      });
+  });
 });
