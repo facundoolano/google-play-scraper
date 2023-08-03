3364f8a54d9409c29cf7d423c1639e66c221cb82
jbigman <jbigman@users.noreply.github.com>
2023-08-03 19:52:49 +0200

    Moved sinon in dev dependencies, updated throttle tests with a 1000ms interval and added comments on sinon usage.

--- test/utils.throttle.js
+++ test/utils.throttle.js
@@ -5,22 +5,27 @@ const assert = require('chai').assert;
 
 describe('Throttle tests', function () {
   let server;
-  this.timeout(15000);
 
+  // Create a fake http server to emulate http call and responses.
   before(function () {
     server = sinon.fakeServer.create();
   });
+
+  // Remove any server responses added in current test suite.
   after(function () {
     server.restore();
   });
 
-  const url = 'https://yesno.wtf/api';
+  const url = 'https://yesno.wtf/api'; // Fake url used in this test, it could be anything.
 
-  it('Should make three requests with 5000ms interval. (Throttle function)', function () {
+  it('Should make three requests with 1000ms interval. (Throttle function)', function () {
+
+    // If there is any http request with specified url, the http call will be handled by the fake server and return specified object
+    // This way we remove the dependance with any http api.
     server.respondWith('GET', url, JSON.stringify({ test: 'this works' }));
     const req = throttled(requestLib, {
       limit: 1,
-      interval: 5000
+      interval: 2000
     });
     return Promise.all([req({ url }), req({ url }), req({ url })])
       .then((response) => response.map(req => new Date(req.headers.date).getTime()))
@@ -28,10 +33,10 @@ describe('Throttle tests', function () {
         const firstAndSecondReq = dates[1] - dates[0];
         const secondAndThirdReq = dates[2] - dates[1];
 
-        assert.isAtLeast(firstAndSecondReq, 5000);
-        assert.isAtMost(firstAndSecondReq, 6500);
-        assert.isAtLeast(secondAndThirdReq, 5000);
-        assert.isAtMost(secondAndThirdReq, 6500);
+        assert.isAtLeast(firstAndSecondReq, 1000);
+        assert.isAtMost(firstAndSecondReq, 2000);
+        assert.isAtLeast(secondAndThirdReq, 1000);
+        assert.isAtMost(secondAndThirdReq, 2000);
       });
   });
 });
