1566fca8a31801510e939c23208608841fe7471b
gjuoun <gjuoun@gmail.com>
2025-05-26 17:35:49 -0400

    use babel

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+npm ci
+
 rm -rf dist 
 mkdir -p dist/esm
 mkdir -p dist/cjs
@@ -8,8 +10,8 @@ mkdir -p dist/cjs
 cp -r index.js index.d.ts lib dist/esm
 cp package.json dist/esm/package.json
 
-# build cjs
-cp -r index.js index.d.ts lib dist/cjs
+# build cjs - use babel to convert to commonjs
+babel index.js --out-file dist/cjs/index.js
+babel lib --out-dir dist/cjs/lib
+cp index.d.ts dist/cjs/index.d.ts
 cp package.cjs.json dist/cjs/package.json
-
-
